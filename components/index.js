8dd73295efec7d9b4cdbaa261993802153f3fabe
Madina Karachai <medinakarachai@mail.ru>
2020-07-13 22:15:37 +0300

    Created react photoApp

---
+++ components/index.js
@@ -0,0 +1,48 @@
+import React,  { Component } from "react";
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: [],
+            file: null,
+        }
+        this.fileInput = React.createRef();
+    }
+
+
+handleSubmit = (e) => {
+e.preventDefault();
+const files = Object.values(this.fileInput.current.files).map(file => URL.createObjectURL(file))
+      this.setState({images: [...files]})
+
+console.log(files);
+
+}
+
+    // handleChange = (e) => {
+    //     e.preventDefault();
+    //     const file = URL.createObjectURL(this.fileInput.current.files[0]);
+    //     this.setState({file})
+    //     console.log('File', file)
+    // }
+createImg = (key, file) => (<img key={key} alt={key} src={file} />)
+render() {
+
+    return (
+        <div>
+        <form className="form" onSubmit={this.handleSubmit}>
+        <input className="form__input" type='file' multiple ref={this.fileInput} />
+        <button className="form__button" type="submit">OK</button>
+        </form>
+            {
+                this.state.images.map((file, i) => this.createImg(i, file))
+            }
+        </div>
+    )
+}
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+console.log("Hello, Madina!");
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './components/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Madina Karachai - PhotoApp',
+            favicon: '',
+            template: path.resolve(__dirname, 'components/index.html'),
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ],
+    },
+};
