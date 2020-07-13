24bda5cd2153bc3952263953f3a604e062dd01c1
Madina Karachai <medinakarachai@mail.ru>
2020-07-13 23:01:20 +0300

    Added eslint and prettier

--- components/index.js
+++ components/index.js
@@ -1,48 +1,52 @@
-import React,  { Component } from "react";
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            images: [],
-            file: null,
-        }
-        this.fileInput = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      file: null,
+    };
+    this.fileInput = React.createRef();
+  }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const files = Object.values(this.fileInput.current.files).map((file) =>
+      URL.createObjectURL(file)
+    );
+    this.setState({ images: [...files] });
 
-handleSubmit = (e) => {
-e.preventDefault();
-const files = Object.values(this.fileInput.current.files).map(file => URL.createObjectURL(file))
-      this.setState({images: [...files]})
+    console.log(files);
+  };
 
-console.log(files);
-
-}
-
-    // handleChange = (e) => {
-    //     e.preventDefault();
-    //     const file = URL.createObjectURL(this.fileInput.current.files[0]);
-    //     this.setState({file})
-    //     console.log('File', file)
-    // }
-createImg = (key, file) => (<img key={key} alt={key} src={file} />)
-render() {
-
+  // handleChange = (e) => {
+  //     e.preventDefault();
+  //     const file = URL.createObjectURL(this.fileInput.current.files[0]);
+  //     this.setState({file})
+  //     console.log('File', file)
+  // }
+  createImg = (key, file) => <img key={key} alt={key} src={file} />;
+  render() {
     return (
-        <div>
+      <div>
         <form className="form" onSubmit={this.handleSubmit}>
-        <input className="form__input" type='file' multiple ref={this.fileInput} />
-        <button className="form__button" type="submit">OK</button>
+          <input
+            className="form__input"
+            type="file"
+            multiple
+            ref={this.fileInput}
+          />
+          <button className="form__button" type="submit">
+            OK
+          </button>
         </form>
-            {
-                this.state.images.map((file, i) => this.createImg(i, file))
-            }
-        </div>
-    )
-}
+        {this.state.images.map((file, i) => this.createImg(i, file))}
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 console.log("Hello, Madina!");
