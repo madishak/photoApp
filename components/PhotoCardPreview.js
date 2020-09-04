0afe9a75a762f6700cb6effeb7d90d5e9ac36003
Madina Karachai <medinakarachai@mail.ru>
2020-09-04 15:18:54 +0300

    Made PhotoCardPreview

--- components/PhotoCardPreview.js
+++ components/PhotoCardPreview.js
@@ -2,30 +2,62 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import Header from './Header';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+  image: {
+    margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: 20,
+    maxWidth: 345,
+    height: 'auto',
+    borderRadius: 5,
+    boxShadow: '0 0 5px #616161',
+  },
+  title: {
+    textAlign: "center",
+    marginTop: 20,
+  },
+  caption: {
+    textAlign: "center",
+  },
+  backButton: {
+    margin: '0 auto',
+    display: 'flex',
+    marginTop: 10,
+  }
+});
+
 
 const mapStateToProps = (state) => {
-  console.log(state.photos.filter(({ id, name, file, description }) => id === state.currentId))
   return {
-    currentId: state.currentId,
-    photo: state.photos.filter(({ id, name, file, description }) => id === state.currentId),
+    photo: state.photos.filter(({ id }) => id === state.currentId),
   };
 };
 
 const PhotoCardPreview = (props) => {
+  const classes = useStyles();
   const history = useHistory();
-  const { currentId, photo } = props;
-  // const 
-  const [{ id, name, file, description }] = photo;
-  console.log("IDDDD", currentId, id, name, file, description);
-return (
-<div>
- <Header />
-
-  Hello {currentId}
-  {name}
-  {description}
+  const { photo } = props;
+  const [{ name, file, description }] = photo;
   
-  </div>);
+  const handleClick = () => history.push('/');
+  return (
+    <div>
+      <Header />
+      <img src={file} className={classes.image} />
+      <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
+        {name}
+      </Typography>
+      <Typography variant="subtitle1" component="p" className={classes.caption}>
+        {description}
+      </Typography>
+      <Button type="button" variant="contained" color="secondary" onClick={handleClick}  className={classes.backButton}>GO BACK</Button>
+    </div>);
 };
 
 export default connect(mapStateToProps)(PhotoCardPreview);
