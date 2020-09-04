5e513c21e10e448f15a31534797e13885875ef42
Madina Karachai <medinakarachai@mail.ru>
2020-09-04 15:18:54 +0300

    Made P hotoCardView

--- components/Home.js
+++ components/Home.js
@@ -2,22 +2,29 @@ import React from "react";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from '@material-ui/core';
 import Header from "./Header";
 import Form from "./Form";
 import PhotoCard from "./PhotoCard";
 import PhotosLoaderButton from "./PhotosLoaderButton";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles({
   photoCardsWrapper: {
     margin: "0 auto",
   },
-}));
+  text: {
+    textAlign: "center",
+    marginTop: 15,
+  },
+});
 
 const Home = () => {
   const classes = useStyles();
   return (
     <div>
       <Header />
+      <Typography display="block" className={classes.text}>Load photos yourself!</Typography>
+      <Typography display="block" className={classes.text}>Or just load from the Internet by clicking pink button.</Typography>
 
       <Grid container justify="center" item xs={12}>
         <Form />
